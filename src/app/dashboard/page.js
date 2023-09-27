b8051651907e8fcbee16b20742d20466cf771c90
Colombo <fulanodetal@gmail.com>
2023-09-26 21:14:27 -0300

    first

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+"use client";
+const dashboard = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <h1>Dashboard</h1>
+        <br />
+      </div>
+    </>
+  );
+};
+
+export default dashboard;
